fc4d541007d71bd03866cabf21b358205fc48b65
Ahmed Ali <bluemax7o1@gmail.com>
2021-09-22 01:58:48 +0200

    remove old app template

--- templates/app/src/start.ts
+++
@@ -1,9 +0,0 @@
-import { RunMode } from '../../../system/core/dist';
-import MainApp from './main';
-
-async function main() {
-  const app = new MainApp();
-  await app.run({ mode: RunMode.APP });
-}
-
-main().catch(console.log);
