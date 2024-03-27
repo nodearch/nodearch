eb251bdd7754e403e55068b3391d70b39f34a704
Ahmed Ali <bluemax7o1@gmail.com>
2024-03-27 22:17:39 +0200

    Update socketio redis interface (#52)
    * update socket io redis interface
    
    * rush change

--- extensions/socket.io-redis/src/interfaces.ts
+++ extensions/socket.io-redis/src/interfaces.ts
@@ -1,13 +1,11 @@
+import { IExtensionProviderComponent } from '@nodearch/core';
 import { ClassConstructor } from '@nodearch/core/utils';
 import { RedisAdapterOptions } from '@socket.io/redis-adapter';
 import { ShardedRedisAdapterOptions } from '@socket.io/redis-adapter/dist/sharded-adapter.js';
 
-export interface IRedisProvider {
-  get(): any;
-}
 
 export interface IRedisAdapterOptions {
-  redisProvider: ClassConstructor<IRedisProvider>;
+  redisClient: IExtensionProviderComponent;
   options?: Partial<RedisAdapterOptions>;
   shardedOptions?: ShardedRedisAdapterOptions;
 }
