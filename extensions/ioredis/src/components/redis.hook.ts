1c1f22a8be1b768a3ba038a8a49154c11a828cbe
Ahmed Ali <bluemax7o1@gmail.com>
2024-04-21 22:52:26 +0200

    Ioredis start fix (#54)
    * fix redis onStart
    
    * rush change

--- extensions/ioredis/src/components/redis.hook.ts
+++ extensions/ioredis/src/components/redis.hook.ts
@@ -1,6 +1,7 @@
 import { Hook, IHook, Logger } from '@nodearch/core';
 import { RedisClient } from './redis.service.js';
 
+
 @Hook({ export: true })
 export class RedisHook implements IHook {
   constructor(
@@ -9,31 +10,20 @@ export class RedisHook implements IHook {
   ) {}
 
   async onStart() {
-    try {
-      await this.connectToRedis();
-      this.logger.info('Connected to Redis');
-    }
-    catch(err) {
-      this.logger.error('Failed to connect to Redis', err);
-      throw err;
-    }
+    await this.connectToRedis();
   }
 
   private async connectToRedis() {
-    await (new Promise((resolve, reject) => {
-      const client = this.redisClient.get();
-      
-      client.on('error', (error) => {
-        reject(error);
-      });
-      
-      client.on('connect', () => {
-        resolve(null);
-      });
-    
-      client.on('ready', () => {
-        resolve(null);
-      });
-    }));
+    const client = this.redisClient.get();
+
+    try {
+      this.logger.info('Connecting to Redis...');
+      await client.ping();
+      this.logger.info('Connected to Redis');
+    }
+    catch(err) {
+      this.logger.error('Failed to connect to Redis', err); 
+      throw err;
+    }
   }
 }
