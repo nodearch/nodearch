76e7efd07308e68c0f76f21b87bba91e798d0f53
Ahmed Ali <bluemax7o1@gmail.com>
2024-05-08 10:31:30 +0300

    Fix/namespace inject (#55)
    * fix namespace inject
    
    * rush change

--- lib/core/src/components/component/ns.decorator.ts
+++ lib/core/src/components/component/ns.decorator.ts
@@ -1,7 +1,9 @@
 import { multiInject } from 'inversify';
+import { Container } from '../../components.index.js';
+
 
 export function Ns(namespace: string): ParameterDecorator {
   return function (target: any, key: string | symbol | undefined, index: number) {
-    multiInject(namespace)(target, <string>key, index);
+    multiInject(Container.getNamespaceId(namespace))(target, <string>key, index);
   }
 }
