63e4cb90c85aba11794c546ec01e73b37881e5a3
Ahmed Ali <bluemax7o1@gmail.com>
2025-01-04 20:24:09 +0200

    Fix prisma export (#67)
    * fix prisma export
    
    * rush change

--- extensions/prisma/src/components/prisma.hook.ts
+++ extensions/prisma/src/components/prisma.hook.ts
@@ -1,4 +1,4 @@
-import { Hook, IHook } from '@nodearch/core';
+import { Hook, IHook, Logger } from '@nodearch/core';
 import { PrismaClient } from './prisma.service.js';
 
 
@@ -6,14 +6,19 @@ import { PrismaClient } from './prisma.service.js';
 export class PrismaHook implements IHook {
 
   constructor(
-    private prisma: PrismaClient
+    private prisma: PrismaClient,
+    private logger: Logger
   ) {}
 
   async onStart() {
+    this.logger.info("Connecting to Prisma database...");
     await this.prisma.connect();
+    this.logger.info("Connected to Prisma database");
   }
 
   async onStop() {
+    this.logger.info("Disconnecting from Prisma database...");
     await this.prisma.disconnect();
+    this.logger.info("Disconnected from Prisma database");
   }
 }
