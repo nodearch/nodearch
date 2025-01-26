6d8caecad479583776837993400482d409d05886
Ahmed Ali <bluemax7o1@gmail.com>
2025-01-26 20:53:30 +0200

    update pkgs

--- lib/cli/src/utils/app-starter.ts
+++ lib/cli/src/utils/app-starter.ts
@@ -1,9 +1,11 @@
+import { register } from 'node:module';
 import { AppLoader, AppLoadMode } from '@nodearch/core/fs';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+import path from 'node:path';
 
-let rootDir = process.argv.find(x => x.startsWith('rootDir='));
+const swcNodePath = fileURLToPath(import.meta.resolve('@swc-node/register'));
+const swcNodeRegisterPath = path.join(path.dirname(swcNodePath), 'esm', 'esm.mjs');
 
-if (rootDir) {
-  rootDir = rootDir.replace('rootDir=', '');
+register(pathToFileURL(swcNodeRegisterPath), pathToFileURL('./'));
 
-  await (new AppLoader({ loadMode: AppLoadMode.TS, cwd: new URL(rootDir), initMode: 'start' })).load();
-}
+await (new AppLoader({ loadMode: AppLoadMode.TS, cwd: pathToFileURL(process.cwd()), initMode: 'start' })).load();
